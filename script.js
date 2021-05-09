bc074808874faf2fe11df3c7ed3ade111743f029
Lara Isak <luckasti.larry@gmail.com>
2021-05-09 20:11:25 +0200

    🎨 create function for adding images to cards

--- script.js
+++ script.js
@@ -29,12 +29,36 @@ var images = [
 ];
 
 var card = document.querySelectorAll(".card");
-for(var i = 0; i < card.length; i++) {
-  // <img src="" alt="">
-  card[i].innerHTML = "<img src='" + images[i] + "' alt='img' class='hidden'>";
-  
-  card[i].addEventListener('click', e => {
-    console.log(e.target);
-  }); 
+
+
+function addImages() {
+  // loop through the divs which represent cards
+  for(var i = 0; i < card.length; i++) {
+    
+    // create a random number between 0 - 11
+    var rand = Math.floor(Math.random() * (images.length-1));
+
+    /* 
+      - add an image element to each card div
+      - image src will be one of the images from images array
+      - rand is used to randomize index of an image from array
+      - innerHTML method can be used in this case because div element doesn't have any prior children
+      - if it did have, the innerHTML() method would replace all prior children with the new child    below, img element
+      - if we just need to insert new element(s) without eplacing the existing ones, we can use 
+      .insertAdjacentHTML() method
+     */
+    card[i].innerHTML = "<img src='" + images[rand] + "' alt='img'>";
+
+    // after an image has been added to the card div, we're removing that image from the images field so we don't have images repeating more than 2 times
+    images.splice(rand, 1);
+  } 
 }
+
+addImages();
+
+
+
+// card.addEventListener('click', e => {
+
+// });
 
